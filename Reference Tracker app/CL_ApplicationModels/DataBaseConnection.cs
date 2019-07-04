b16e6009f6a55f2fd361c4654d7df1bd3c86e9cd
Fahad-Md-Kamal <faahad.hossain@gmail.com>
2019-07-04 11:07:21 +0600

    Modified Source code

--- Reference Tracker app/CL_ApplicationModels/DataBaseConnection.cs
+++ Reference Tracker app/CL_ApplicationModels/DataBaseConnection.cs
@@ -6,6 +6,6 @@
     /// </summary>
     public class DataBaseConnection
     {
-        public static string DataAccess = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Activities\Study\L4DC 2018 Assignments\Dec 18\DDOOCP\00171328_Fahad_Md_Kamal_DDooCP_December_18\Reference Tracker app\ArticalReferencingAndCitation\ArticalReferencingAndCitation.mdf;Integrated Security=True";
+        public static string DataAccess = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=G:\Works\Article-Referencing-And-Citation-master\Article-Referencing-And-Citation-master\Reference Tracker app\ArticalReferencingAndCitation\ArticalReferencingDb.mdf;Integrated Security=True";
     }
 }
